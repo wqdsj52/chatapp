f509ea34a8d2b90f9a70912e71df51297338d997
wqdsj52 <wqdsj52@users.noreply.github.com>
2026-06-28 22:12:21 +0800

    feat: add PWA support for Android install

---
+++ apps/web/public/service-worker.js
@@ -0,0 +1,14 @@
+﻿self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
